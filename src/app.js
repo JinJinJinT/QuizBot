80897501ded4e5d245ec6636fdf935697a3c3ce7
marduk5 <marduk5@users.noreply.github.com>
2020-10-17 16:42:05 -0700

    using tmi.js

--- src/app.js
+++ src/app.js
@@ -1,1 +1,21 @@
-console.log("app started")
+import tmi from 'tmi.js'
+
+const client = new tmi.Client({
+	options: { debug: true },
+	connection: {
+		reconnect: true,
+		secure: true
+	},
+	identity: {
+		username: 'quizbot',
+		password: 'oauth:jd8o89z5ucuzps1ooxmyornq4m7owh'
+	},
+	channels: [ 'j_xx_n' ]
+});
+client.connect().catch(console.error);
+client.on('message', (channel, userstate, message, self) => {
+	if(self) return;
+	if(message.toLowerCase() === '!hello') {
+		client.say(channel, `@${userstate.username}, heya!`);
+	}
+});
