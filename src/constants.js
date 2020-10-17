b3f73c8f9af6d608305a412d22f76841a94fb264
JinJinJinT <60024162+JinJinJinT@users.noreply.github.com>
2020-10-17 16:47:46 -0700

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,3 @@
-export const CHANNEL_NAME = '#j_xx_n';
-export const OAUTH_TOKEN = 'oauth:jd8o89z5ucuzps1ooxmyornq4m7owh';
-export const BOT_USERNAME = 'j_xx_n';
+export const CHANNEL_NAME = '#yourname';
+export const OAUTH_TOKEN = 'oauth:yourtoken';
+export const BOT_USERNAME = 'botname';
