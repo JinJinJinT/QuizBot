44248c31a0d5a3d5439d85da950c878ae2fe1a4a
marduk5 <marduk5@users.noreply.github.com>
2020-10-17 20:52:58 -0700

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 export const CHANNEL_NAME = '#j_xx_n';
 export const OAUTH_TOKEN = 'oauth:jd8o89z5ucuzps1ooxmyornq4m7owh';
 export const BOT_USERNAME = 'j_xx_n';
@@ -6,8 +5,3 @@ export const KEYWORDS = [
     '!createquiz:',
     '!answer:'
 ]
-=======
-export const CHANNEL_NAME = '#yourname';
-export const OAUTH_TOKEN = 'oauth:yourtoken';
-export const BOT_USERNAME = 'botname';
->>>>>>> 5a18d344aa735fa2e9315a76caa6367b965e6346
